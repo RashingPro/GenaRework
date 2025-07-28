355721ce532e36785724c96ebba6b88b036c8a86
RashingPro <rashingpro@yandex.ru>
2025-07-29 01:41:23 +0300

    refactor: small changes

--- src/bot/events/memberJoin.ts
+++ src/bot/events/memberJoin.ts
@@ -1,11 +1,11 @@
 import { ArgsOf, Discord, On } from "discordx";
-import config from "../../../config.json";
+import config from "@/../config.json";
 
 @Discord()
 export abstract class MemberJoin {
     @On({ event: "guildMemberAdd" })
     async onMemberAdd([member]: ArgsOf<"guildMemberAdd">) {
-        if (!member.guild.systemChannelId) return;
+        if (!member.guild.systemChannel) return;
         const channel = member.guild.systemChannel;
         if (!channel || !channel.isSendable()) return;
         const msg = config.messages.join[Math.floor(Math.random() * config.messages.join.length)];
--- src/bot/events/memberLeave.ts
+++ src/bot/events/memberLeave.ts
@@ -1,14 +1,14 @@
-import { ArgsOf, Client, Discord, On } from "discordx";
+import { ArgsOf, Discord, On } from "discordx";
 import config from "@/../config.json";
 
 @Discord()
 export abstract class MemberLeave {
     @On({ event: "guildMemberRemove" })
     async onMemberRemove([member]: ArgsOf<"guildMemberRemove">) {
-        if (!member.guild.systemChannelId) return;
-        const channel = await member.guild.channels.fetch(member.guild.systemChannelId);
-        const msg = config.messages.leave[Math.floor(Math.random() * config.messages.leave.length)];
+        if (!member.guild.systemChannel) return;
+        const channel = member.guild.systemChannel;
         if (!channel || !channel.isSendable()) return;
+        const msg = config.messages.leave[Math.floor(Math.random() * config.messages.leave.length)];
         await channel.send(msg.replace("{0}", `<@${member.id}>`));
     }
 }
