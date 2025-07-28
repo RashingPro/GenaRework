c7b56a969ce198a70cbf1639c4310c0dba91b740
RashingPro <rashingpro@yandex.ru>
2025-07-28 21:11:29 +0300

    feat: add blank bot files

---
+++ src/bot/index.ts
@@ -0,0 +1,33 @@
+import { Client } from "discordx";
+import { importx } from "@discordx/importer";
+import { IntentsBitField } from "discord.js";
+
+export default class Bot {
+    constructor() {
+        this.client = new Client({
+            intents: [
+                IntentsBitField.Flags.Guilds,
+                IntentsBitField.Flags.GuildMessages,
+                IntentsBitField.Flags.GuildMessageReactions,
+                IntentsBitField.Flags.GuildMembers
+            ],
+            silent: false
+        });
+
+        this.client.once("ready", async () => {
+            await this.client.initApplicationCommands();
+            console.log("✅ Bot is ready!");
+        });
+
+        this.client.on("interactionCreate", async interaction => {
+            await this.client.executeInteraction(interaction);
+        });
+    }
+
+    public readonly client;
+
+    async start(token: string) {
+        await importx(`${__dirname}/bot/commands/**/*.{js,ts}`);
+        await this.client.login(token);
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import Bot from "@/bot";
+
+dotenv.config();
+
+async function main() {
+    const bot = new Bot();
+
+    if (!process.env.TOKEN)
+        throw new Error()
+
+    await bot.start(process.env.TOKEN)
+}
+
+main()
