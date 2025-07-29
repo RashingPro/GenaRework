5d59258b82abe60a69007fb5b1fa839953243afa
RashingPro <rashingpro@yandex.ru>
2025-07-30 00:51:57 +0300

    feat: implement remove role when remove reaction in reaction-role system
    Also refactor code to reduce duplicated parts

--- src/bot/events/onReaction.ts
+++ src/bot/events/onReaction.ts
@@ -1,5 +1,6 @@
 import Logger from "@/logger";
 import config from "config.json";
+import { MessageReaction, PartialMessageReaction } from "discord.js";
 import { ArgsOf, Discord, On } from "discordx";
 import { inject, injectable } from "tsyringe";
 
@@ -10,6 +11,23 @@ export class OnReaction {
 
     @On({ event: "messageReactionAdd" })
     async onReactionAdd([reaction]: ArgsOf<"messageReactionAdd">) {
+        const roleId = await this.handleReaction(reaction);
+        if (!roleId) return;
+        await reaction.message.fetch();
+        if (!reaction.message.member) return;
+        await reaction.message.member.roles.add(roleId);
+    }
+
+    @On({ event: "messageReactionRemove" })
+    async onReactionRemove([reaction]: ArgsOf<"messageReactionRemove">) {
+        const roleId = await this.handleReaction(reaction);
+        if (!roleId) return;
+        await reaction.message.fetch();
+        if (!reaction.message.member) return;
+        await reaction.message.member.roles.remove(roleId);
+    }
+
+    private async handleReaction(reaction: MessageReaction | PartialMessageReaction) {
         const configChannels = config.channels.roles as Record<string, Record<string, Record<string, string>>>;
         if (!Object.keys(configChannels).includes(reaction.message.channel.id)) return;
         const configMessages = configChannels[reaction.message.channel.id];
@@ -17,9 +35,6 @@ export class OnReaction {
         const configRoles = configMessages[reaction.message.id];
         if (!Object.keys(configRoles).includes(reaction.emoji.toString())) return;
 
-        const roleId = configRoles[reaction.emoji.toString()];
-        await reaction.message.fetch();
-        if (!reaction.message.member) return;
-        await reaction.message.member.roles.add(roleId);
+        return configRoles[reaction.emoji.toString()];
     }
 }
