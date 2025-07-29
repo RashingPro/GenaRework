71c7b1a273606183f50f80c249fe7642c26bf770
RashingPro <rashingpro@yandex.ru>
2025-07-30 00:38:57 +0300

    fix(di): top-level import now works fine
    move index.ts to main.ts, and add `import "reflect-metadata` to index.ts to prevent prettier from moving this import from first line
    BREAKING CHANGE: index.ts content is now in main.ts

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import Bot from "@/bot";
+import Logger from "@/logger";
+import { tsyringeDependencyRegistryEngine } from "@discordx/di";
+import { DIService } from "discordx";
+import dotenv from "dotenv";
+import { container } from "tsyringe";
+
+dotenv.config();
+
+async function main() {
+    const logger = new Logger();
+    container.register(Logger, { useValue: logger });
+    DIService.engine = tsyringeDependencyRegistryEngine.setInjector(container);
+
+    const token = process.env.TOKEN;
+    if (!token) {
+        throw new Error("Token not found. Please check .env file");
+    }
+
+    const bot = new Bot(logger);
+    try {
+        await bot.start(token);
+    } catch (error) {
+        await logger.error("Caught runtime error:", error);
+    }
+}
+
+void main();
