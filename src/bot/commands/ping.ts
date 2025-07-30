78ac50d82d46c49d45f2af21851c4e92c916a7f5
RashingPro <rashingpro@yandex.ru>
2025-07-30 14:06:29 +0300

    feat: make `/ping` more responsible

--- src/bot/commands/ping.ts
+++ src/bot/commands/ping.ts
@@ -8,10 +8,15 @@ export class Ping {
     @Slash({ name: "ping", description: "Some cool text goes here" })
     @Guard(IsOnAllowedGuild)
     async ping(interaction: CommandInteraction, client: Client) {
+        let websocketPing: string | number = Math.round(client.ws.ping);
+        const interactionPing = Date.now() - interaction.createdTimestamp
+
+        websocketPing = websocketPing < 0 ? "WebSocket connection is not established yet. Check logs for details" : websocketPing
+
         const embed = new EmbedBuilder()
             .setColor(0x0099ff)
             .setTitle(`${config.emojis.ping_pong} Pong!`)
-            .setDescription(`Bot latency: ${client.ws.ping}ms`);
+            .addFields({name: "WebSocket ping", value: `${websocketPing}ms`}, {name: "Interaction delay (may be wrong)", value: `${interactionPing}ms`});
         await interaction.reply({ embeds: [embed] });
     }
 }
